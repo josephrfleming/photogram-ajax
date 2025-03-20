ddda3a7e23f73ab607d28edad52680f029231c8d
Joseph Fleming <162403379+josephrfleming@users.noreply.github.com>
2025-03-20 23:38:34 +0000

    start

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,69 +1,54 @@
 class LikesController < ApplicationController
-  before_action :set_like, only: %i[ show edit update destroy ]
+  before_action :authorize_create, only: [:create]
+  before_action :set_like, only: [:destroy]
+  before_action :authorize_destroy, only: [:destroy]
 
-  # GET /likes or /likes.json
-  def index
-    @likes = Like.all
-  end
-
-  # GET /likes/1 or /likes/1.json
-  def show
-  end
-
-  # GET /likes/new
-  def new
-    @like = Like.new
-  end
-
-  # GET /likes/1/edit
-  def edit
-  end
-
-  # POST /likes or /likes.json
+  # POST /likes
   def create
     @like = Like.new(like_params)
+    @like.fan = current_user  # Ensure the like is always by the logged-in user
 
-    respond_to do |format|
-      if @like.save
-        format.html { redirect_back fallback_location: @like.photo, notice: "Like was successfully created." }
-        format.json { render :show, status: :created, location: @like }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @like.errors, status: :unprocessable_entity }
-      end
+    if @like.save
+      redirect_back fallback_location: @like.photo, notice: "Like was successfully created."
+    else
+      redirect_back fallback_location: root_url, alert: "Unable to create like."
     end
   end
 
-  # PATCH/PUT /likes/1 or /likes/1.json
-  def update
-    respond_to do |format|
-      if @like.update(like_params)
-        format.html { redirect_to @like, notice: "Like was successfully updated." }
-        format.json { render :show, status: :ok, location: @like }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @like.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /likes/1 or /likes/1.json
+  # DELETE /likes/1
   def destroy
     @like.destroy
-    respond_to do |format|
-      format.html { redirect_back fallback_location: @like.photo, notice: "Like was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_back fallback_location: @like.photo, notice: "Like was successfully destroyed."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_like
       @like = Like.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def like_params
-      params.require(:like).permit(:fan_id, :photo_id)
+      # We remove :fan_id from the strong parameters so it can't be overridden.
+      params.require(:like).permit(:photo_id)
+    end
+
+    # Only allow a user to like a photo if:
+    #  - They are the owner, OR
+    #  - The photo's owner is public, OR
+    #  - They are following the photo's owner
+    def authorize_create
+      photo = Photo.find(params[:like][:photo_id])
+
+      if current_user != photo.owner && photo.owner.private? && !current_user.leaders.include?(photo.owner)
+        redirect_back fallback_location: root_url, alert: "Not authorized to like this photo."
+      end
+    end
+
+    # Only allow the fan (or possibly the photo owner) to remove the like.
+    # This example allows only the fan to destroy their own like.
+    def authorize_destroy
+      unless current_user == @like.fan
+        redirect_back fallback_location: root_url, alert: "Not authorized to remove this like."
+      end
     end
 end
